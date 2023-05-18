2132d3be0ae3bd2a55cb8cc9c0d1c6e0707d2f28
suryaselvakumar123 <suryaselvakumar1007@gmail.com>
2023-05-18 20:38:17 +0530

    commit all

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -1,30 +1,26 @@
-
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-
 
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 const passwordResetRoute = express.Router();
 
-
 passwordResetRoute.post('/', async (req, res) => {
     const { token, password } = req.body;
 
-
     try {
         const salt = await bcrypt.genSalt();
-        const hashedNewPassword = await bcrypt.hash(newPassword, salt);
+        const hashedNewPassword = await bcrypt.hash(password, salt);
 
-        const user = await User.findOne({ secretCode });
+        const user = await User.findOne({ secretCode: token });
 
         if (!user) {
             return res.status(400).send("User does not exist");
         }
 
         await User.findOneAndUpdate(
-            { secretCode },
+            { secretCode: token },
             { password: hashedNewPassword, secretCode: "" }
         );
 
